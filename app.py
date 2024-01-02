f80f4d5dabf9e09a7081386ffc40369ef0b94c14
Esli Adarkwah <esliadarkwah@gmail.com>
2024-01-02 01:21:04 +0000

    Completed Arithmetic Operations of all Function
    Added subFunction
    Added divFunction
    Added prodFunction
    Added powFunction

--- app.py
+++ app.py
@@ -1,2 +1,15 @@
 def addFunction(x,y):
     return x + y
+
+def subFunction(x,y):
+    return x - y
+
+def divFunction(x,y):
+    return x/y
+
+def prodFunction(x,y):
+    return x*y
+
+def powFunction(x,y):
+    return x**y
+    
